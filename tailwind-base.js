40cfd1a51efc00336872e3a1cdd0f4b688bf4627
Benjamin Barbe <benjamin@pandasuite.com>
2021-04-08 18:23:02 +0200

    Tailwind configuration file

---
+++ tailwind-base.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontSize: {
+      xs: '.625rem',
+      sm: '.75rem',
+      base: '.875rem',
+      lg: '1rem',
+      xl: '1.125rem',
+      '2xl': '1.25rem',
+      '3xl': '1.5rem',
+      '4xl': '1.875rem',
+      '5xl': '2.25rem',
+      '6xl': '3rem',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
