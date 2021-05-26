25953faf222942b1fde9b07adb33ba6f97a7fa19
Benjamin Barbe <benjamin@pandasuite.com>
2021-05-26 14:44:35 +0200

    Tailwind config files

---
+++ tailwind.components.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  '.btn-primary': {
+    '@apply inline-flex items-center py-1 px-5 text-sm font-medium rounded text-white bg-green-500 hover:bg-green-600 focus:outline-none transition ease-in-out duration-150': {},
+  },
+  '.btn': {
+    '@apply inline-flex items-center py-1 px-5 text-sm font-medium rounded text-purple-600 bg-gray-100 hover:bg-gray-200 focus:outline-none transition ease-in-out duration-150': {},
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+      './public/index.html',
+      './node_modules/pandasuite-bridge/tailwind*.{js,jsx,ts,tsx}',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontSize: {
+      xs: '.625rem',
+      sm: '.75rem',
+      base: '.875rem',
+      lg: '1rem',
+      xl: '1.125rem',
+      '2xl': '1.25rem',
+      '3xl': '1.5rem',
+      '4xl': '1.875rem',
+      '5xl': '2.25rem',
+      '6xl': '3rem',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
