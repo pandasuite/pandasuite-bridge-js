915b18d94dbffe948687fe254b6277d52fb9d479
Benjamin Barbe <benjamin@pandasuite.com>
2021-06-02 14:43:35 +0200

    Purge only in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: [
-      './src/**/*.{js,jsx,ts,tsx}',
-      './public/index.html',
-      './node_modules/pandasuite-bridge/tailwind*.{js,jsx,ts,tsx}',
-    ],
-  },
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    './node_modules/pandasuite-bridge/tailwind*.{js,jsx,ts,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
